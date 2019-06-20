f4bb47e3a234fe9a7e4c2c7604b9a0d7311d587c
DjTrololo <alberto.abad.sanz@gmail.com>
2019-06-20 18:32:00 +0200

    no message

--- core/src/com/libgdx/spaceshooter/SoundManager.java
+++ core/src/com/libgdx/spaceshooter/SoundManager.java
@@ -15,6 +15,27 @@ public class SoundManager {
     //FOR SIMPLE SOUNDS
     //public static Sound Shoot = Gdx.audio.newSound(Gdx.files.internal(""));
 
+    public static Sound pShotN = Gdx.audio.newSound(Gdx.files.internal("PLAYER_SHOT.wav"));
+    public static Sound pShotS = Gdx.audio.newSound(Gdx.files.internal("PLAYER_SHOTSPECIAL.wav"));
+    public static Sound pBomb = Gdx.audio.newSound(Gdx.files.internal("PLAYER_BOMB.wav"));
+    public static Sound pExp = Gdx.audio.newSound(Gdx.files.internal("PLAYER_BOMB.wav"));
+    public static Sound pRespawn = Gdx.audio.newSound(Gdx.files.internal("PLAYER_RESPAWN.wav"));
+
+    public static Sound seShot = Gdx.audio.newSound(Gdx.files.internal("SE_SHOT"));
+    public static Sound seExpl = Gdx.audio.newSound(Gdx.files.internal("EXPLOSION_SIMPLE.wav"));
+    public static Sound aeShot = Gdx.audio.newSound(Gdx.files.internal("AE_SHOT"));
+    public static Sound aeExpl = Gdx.audio.newSound(Gdx.files.internal("EXPLOSION_ADVANCED.wav"));
+    public static Sound ovniLaser = Gdx.audio.newSound(Gdx.files.internal("OVNI_LASERLOOP.wav"));
+    public static Sound ovniNormal = Gdx.audio.newSound(Gdx.files.internal("OVNI_NORMAL.wav"));
+    public static Sound ovniSoftExp = Gdx.audio.newSound(Gdx.files.internal("EXPLOSION_SOFTOVNI.wav"));
+    public static Sound ovniHardExp = Gdx.audio.newSound(Gdx.files.internal("EXPLOSION_HARDOVNI.wav"));
+    public static Sound ovniFinalExp = Gdx.audio.newSound(Gdx.files.internal("EXPLOSION_FINALOVNI.wav"));
+
+    public static Sound uiHit = Gdx.audio.newSound(Gdx.files.internal("UI_HIT.wav"));
+    public static Sound uiLifeLoss = Gdx.audio.newSound(Gdx.files.internal("UI_LIFELOSS.wav"));
+    public static Sound uiMove = Gdx.audio.newSound(Gdx.files.internal("UI_MOVE.wav"));
+    public static Sound uiSelect = Gdx.audio.newSound(Gdx.files.internal("UI_SELECT.wav"));
+
 
 
     public static void reproduceSounds(int soundID){
